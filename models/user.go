eb414066c5ebc8c2f208e6a110eb78188fbcd2ed
astianmuchui <astianmuchui@gmail.com>
2024-11-21 20:21:26 +0300

    add models package

---
+++ models/user.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"gorm.io/gorm"
+	"gorm.io/driver/sqlite"
+	"log"
+)
+
+var db *gorm.DB;
+
+type User struct {
+	gorm.Model
+	Username string
+	Email string
+	Password string
+}
+
+func connect() {
+	var err error;
+
+	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Unable to connect to database")
+	}
+}
+
+func CreateUser(user_data *User) (*gorm.DB) {
+	connect() // Connect to database
+
+	result := db.Create(&User{
+			Username: user_data.Username,
+			Email: user_data.Email,
+			Password: user_data.Password,
+		})
+
+	return result
+}
