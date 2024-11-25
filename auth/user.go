a9285662a1b039722939bb97d39bfbac7ca1ec84
astianmuchui <astianmuchui@gmail.com>
2024-11-25 10:08:31 +0300

    Bugfix

--- auth/user.go
+++ auth/user.go
@@ -2,21 +2,26 @@ package auth
 
 import (
 	"github.com/astianmuchui/go-auth/models"
+	"log"
 )
 
 func Login(user *models.User) (bool) {
 	var u models.User
 	models.Connect()
 
-	result := models.DB.First(&u, "username = ?", u.Username)
+	result := models.DB.First(&u, "username = ?", user.Username)
 	if result.RowsAffected == 0 {
+		log.Println("User not found")
 		return (false)
 	} else {
 		// Verify password
 		if models.Password_verify(u.Password, []byte(user.Password)) == true {
+			log.Println("Login successful")
 			return (true)
+		} else {
+			log.Fatal("Login not successful")
+
+			return (false)
 		}
 	}
-
-	return (false)
 }
